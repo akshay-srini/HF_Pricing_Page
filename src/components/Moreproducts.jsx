d470c2bed06025b3055256e637d6fcf52db8507a
Akshay S <100524148+akshay-srini@users.noreply.github.com>
2024-01-10 12:09:19 +0530

    bug fix and transition in FAQ section

--- src/components/Moreproducts.jsx
+++ src/components/Moreproducts.jsx
@@ -5,7 +5,7 @@ export default function Moreporducts() {
         <section className="more-products-section">
             <h2 className="more-products-title">More From HappyFox</h2>
             <div className="more-products-container-spacing">
-                <div className="container-box spacing-in-box">
+                <div className="more-products-container spacing-in-box">
                     <img src="https://assets.www.happyfox.com/v2/images/pricing-lc.svg" alt="" className="more-products-img"/>
                     <h3 className="more-products-name">Live Chat</h3>
                     <p className="more-products-details">Chat with your customers instantly on mobile, desktop, and web.</p>
@@ -14,7 +14,7 @@ export default function Moreporducts() {
                         <IoIosArrowForward className='arrow' />
                     </div>
                 </div>
-                <div className="container-box spacing-in-box">
+                <div className="more-products-container spacing-in-box">
                     <img src="https://assets.www.happyfox.com/v2/images/pricing-cb.svg" alt="" className="more-products-img"/>
                     <h3 className="more-products-name">Chatbot</h3>
                     <p className="more-products-details">Engage and reach your customers 24x7 with ready-to-go chatbots.</p>
@@ -22,7 +22,7 @@ export default function Moreporducts() {
                         <a href="">Get a demo</a>
                         <IoIosArrowForward className='arrow' />
                     </div>
-                </div><div className="container-box spacing-in-box">
+                </div><div className="more-products-container spacing-in-box">
                     <img src="https://assets.www.happyfox.com/v2/images/pricing-ai.svg" alt="" className="more-products-img"/>
                     <h3 className="more-products-name">Assist AI</h3>
                     <p className="more-products-details">Provide IT support directly from Slack and Microsoft Teams.</p>
@@ -30,7 +30,7 @@ export default function Moreporducts() {
                         <a href="">Get a demo</a>
                         <IoIosArrowForward className='arrow' />
                     </div>
-                </div><div className="container-box spacing-in-box">
+                </div><div className="more-products-container spacing-in-box">
                     <img src="https://assets.www.happyfox.com/v2/images/pricing-bi.svg" alt="" className="more-products-img"/>
                     <h3 className="more-products-name">Business Intelligence</h3>
                     <p className="more-products-details">Turn insight into advantage with cross-functional reports & dashboards.</p>
@@ -38,7 +38,7 @@ export default function Moreporducts() {
                         <a href="">Get a demo</a>
                         <IoIosArrowForward className='arrow' />
                     </div>
-                </div><div className="container-box spacing-in-box">
+                </div><div className="more-products-container spacing-in-box">
                     <img src="https://assets.www.happyfox.com/v2/images/pricing-wa.svg" alt="" className="more-products-img"/>
                     <h3 className="more-products-name">Workflow Automation</h3>
                     <p className="more-products-details">Reduce the grunt work with end-to-end process automation.</p>
